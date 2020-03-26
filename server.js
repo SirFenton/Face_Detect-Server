985d0cf52e8ca184eb7ee7d22a3f25f6119809ee
Jaco Alberts <jaco.alberts@gmail.com>
2020-03-26 11:24:54 +0200

    database changes

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const image = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-      host : '127.0.0.1',       //this ip is localhost
+      host : 'postgresql-amorphous-91763',       //this ip is localhost (127.0.0.1)
       user : 'postgres',
       password : 'postgres@1',
       database : 'face_detect'
